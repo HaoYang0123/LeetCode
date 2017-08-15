d0029bf9d43283a5e2f306229ca5c558bc735f7d
HaoYang0123 <yhloverain@gmail.com>
2017-08-15 21:00:11 +0800

    Create BinaryTreeInorderIter.cpp

---
+++ BinaryTreeInorderIter.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> res;
+        stack<TreeNode*> sta;
+        TreeNode* pCurrent = root;
+        while(!sta.empty() || pCurrent != NULL) {
+            if(pCurrent != NULL) {
+                sta.push(pCurrent);
+                pCurrent = pCurrent->left;
+            }
+            else {
+                pCurrent = sta.top();
+                sta.pop();
+                res.push_back(pCurrent->val);
+                pCurrent = pCurrent->right;
+            }
+        }
+        return res;
+    }
+
+};
