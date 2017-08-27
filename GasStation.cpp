d64c12130f6a70d59bec558e7cc887b9dcc68c77
HaoYang0123 <yhloverain@gmail.com>
2017-08-27 12:29:04 +0800

    Update GasStation.cpp

--- GasStation.cpp
+++ GasStation.cpp
@@ -2,6 +2,7 @@
 class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) { //O(n)时间复杂度，O(1)空间复杂度
+        //可以将gas[i]-cost[i]作为一个新的data[i]，在data数组中找最大的连续子序列求和。如果整个序列求和为负数，那么没有解；如果中间的求和为负数，则重新计算新的起点
         int start = 0, total1 = 0, total2 = 0, tank = 0; //tank记录当前还剩多少汽油
         for(int i = 0; i < gas.size(); ++i) {
             tank += gas[i] - cost[i];
