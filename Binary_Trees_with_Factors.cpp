4ea59502be5441d52d373887da4e08aa3862d18b
HaoYang0123 <yhloverain@gmail.com>
2018-10-29 21:27:22 +0800

    Update Binary_Trees_with_Factors.cpp

--- Binary_Trees_with_Factors.cpp
+++ Binary_Trees_with_Factors.cpp
@@ -1,28 +1,17 @@
-//Leetcode 823 
-//没有AC，但思路没啥问题呀！
+//Leetcode 823
+//动态规划
 
 class Solution {
 public:
     int numFactoredBinaryTrees(vector<int>& A) {
-        int mod = 10^9+7;
-        unordered_map<int,int> map, dp; 
-        vector<int> dp2(A.size(),1);
+        unsigned long long mod = 1000000007;
+        unordered_map<int,int> map;
+        unordered_map<unsigned long long, int> dp;
+        sort(A.begin(),A.end());
         //map[i]表示值为i的在A的索引号，dp[i]表示以i为根节点的树的个数
         for(int i=0;i<A.size();++i) {
             map[A[i]] = i;
             dp[A[i]] = 0;
-        }
-        sort(A.begin(),A.end());
-        
-        for(int i=0;i<A.size();++i) {
-            for(int j=0;j<i;++j) {
-                if(A[i]%A[j]==0) {
-                    int right=A[i]/A[j];
-                    if(map.find(right) != map.end()) {
-                        dp2[i] = (dp2[i] + dp2[j]*dp2[map[right]]) % mod;
-                    }
-                }
-            }
         }
         
         unsigned long long res = 0;
@@ -30,23 +19,19 @@ public:
             res += (unsigned long long)(get_number(A[i],i,A,map,dp,mod));
             res = res % mod;
         }
-        for(int i=0;i<A.size();++i) {
-            //cout<<A[i]<<"\t"<<dp[A[i]]<<"\t"<<dp2[A[i]]<<endl;
-            if(dp[A[i]] != dp2[i]) cout<<i<<"\t"<<dp[A[i]]<<"\t"<<dp2[i]<<endl;
-        }
-        return res;
+        return (int)res;
     }
-    int get_number(int val, int index, vector<int> & A, unordered_map<int,int> & map, unordered_map<int,int> & dp, int mod) {
+    unsigned long long get_number(int val, int index, vector<int> & A, unordered_map<int,int> & map, unordered_map<unsigned long long,int> & dp, unsigned long long mod) {
         if(dp[val] != 0) return dp[val];
         unsigned long long res = 1;
         for(int i=index-1;i>=0;--i) {
             if(val % A[i] != 0) continue;
             int a = A[i], b = val/A[i];
             if(map.find(b) == map.end()) continue;
-            int res1 = get_number(a, i, A, map, dp, mod);
-            int res2 = get_number(b, map[b], A, map, dp, mod);
-            res += ((unsigned long long)res1*(unsigned long long)res2)%(mod);
-            res = res % mod;
+            unsigned long long res1 = get_number(a, i, A, map, dp, mod);
+            unsigned long long res2 = get_number(b, map[b], A, map, dp, mod);
+            res = (unsigned long long)res + ((unsigned long long)res1*(unsigned long long)res2)%((unsigned long long)mod);
+            res = res % (unsigned long long)mod;
         }
         dp[val] = res;
         return res;
