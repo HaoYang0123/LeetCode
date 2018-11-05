ef53bb71903d6e4c0121a553dd7b6ed7e3c38dd5
HaoYang0123 <yhloverain@gmail.com>
2018-11-05 22:35:52 +0800

    Create Max_Chunks_to_Make_Sorted.cpp

---
+++ Max_Chunks_to_Make_Sorted.cpp
@@ -0,0 +1,43 @@
+//Leetcode 769
+//动态规划，时间复杂度有些高O(n^3)，网上有O(n)的代码
+
+class Solution {
+public:
+    int maxChunksToSorted(vector<int>& arr) {
+        int n=arr.size();
+        vector<vector<pair<int,int>>> dp(n+1,vector<pair<int,int>>(n+1,pair<int,int>(0,0)));
+        for(int i=0;i<n;++i) {
+            int min=arr[i],max=arr[i];
+            dp[i][i].first=min;
+            dp[i][i].second=max;
+            for(int j=i+1;j<n;++j) {
+                if(arr[j]>max) {
+                    max=arr[j];
+                }
+                else if(arr[j]<min) {
+                    min=arr[j];
+                }
+                dp[i][j].first=min;
+                dp[i][j].second=max;
+            }
+        }
+        vector<vector<int>> dp2(n+1,vector<int>(n+1,0));
+        return dfs(dp,dp2,0,n-1);
+    }
+    
+    int dfs(vector<vector<pair<int,int>>> & dp, vector<vector<int>> & dp2, int start, int end) {
+        if(start==end) return 1;
+        if(dp2[start][end] != 0) return dp2[start][end];
+        int res = 1;
+        for(int i=start;i<end;++i) {
+            int max=dp[start][i].second;
+            int min=dp[i+1][end].first;
+            if(max<min) {
+                int tmp = dfs(dp,dp2,start,i) + dfs(dp,dp2,i+1,end);
+                if(tmp>res) res=tmp;
+            }
+        }
+        dp2[start][end] = res;
+        return res;
+    }
+};
