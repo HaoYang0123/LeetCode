5b638666c4dccd039948713a8097feb57659fa56
HaoYang0123 <yhloverain@gmail.com>
2019-01-23 22:12:37 +0800

    Create Max_Width_Ramp.cpp

---
+++ Max_Width_Ramp.cpp
@@ -0,0 +1,19 @@
+//Leetcode 962
+//网上，找的O(n)的时间复杂度
+
+class Solution {
+public:
+    int maxWidthRamp(vector<int>& A) {
+        stack<int> s;
+        int res = 0, n = A.size();
+        for (int i = 0; i < n; ++i)
+            if (s.empty() || A[s.top()] > A[i])
+                s.push(i);
+        for (int i = n - 1; i > res; --i)
+            while (!s.empty() && A[s.top()] <= A[i]) {
+                res = max(res, i - s.top());
+                s.pop();
+            }
+        return res;
+    }
+};
