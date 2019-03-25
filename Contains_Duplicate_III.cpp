b7743fe786eb51472fc72083adb39f1190c09735
HaoYang0123 <yhloverain@gmail.com>
2019-03-25 18:01:36 +0800

    Create Contains_Duplicate_III.cpp

---
+++ Contains_Duplicate_III.cpp
@@ -0,0 +1,15 @@
+//Leetcode 220
+//O(N*LogK)的时间复杂度，从网上找的代码。
+
+bool containsNearbyAlmostDuplicate(vector<int>& nums, int k, int t) {
+    set<int> window; // set is ordered automatically 
+    for (int i = 0; i < nums.size(); i++) {
+        if (i > k) window.erase(nums[i-k-1]); // keep the set contains nums i j at most k
+        // |x - nums[i]| <= t  ==> -t <= x - nums[i] <= t;
+        auto pos = window.lower_bound(nums[i] - t); // x-nums[i] >= -t ==> x >= nums[i]-t 
+        // x - nums[i] <= t ==> |x - nums[i]| <= t    
+        if (pos != window.end() && *pos - nums[i] <= t) return true;
+        window.insert(nums[i]);
+    }
+    return false;
+}
