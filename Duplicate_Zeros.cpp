38c1232dcf4ec03bc6fce5b3f076d38b080cc13e
HaoYang0123 <yhloverain@gmail.com>
2019-06-17 21:46:03 +0800

    Create Duplicate_Zeros.cpp

---
+++ Duplicate_Zeros.cpp
@@ -0,0 +1,15 @@
+//Leetcode 1089
+
+class Solution {
+public:
+    void duplicateZeros(vector<int>& A) {
+        int n = A.size(), j = n + count(A.begin(), A.end(), 0);
+        //cout<<j<<endl;
+        for (int i = n - 1; i >= 0; --i) {
+            if (--j < n)
+                A[j] = A[i];
+            if (A[i] == 0 && --j < n)
+                A[j] = 0;
+        }
+    }
+};
