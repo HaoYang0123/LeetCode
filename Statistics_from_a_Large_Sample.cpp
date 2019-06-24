9d1918fcd992b23892ec5c67d3eaee345e565a11
HaoYang0123 <yhloverain@gmail.com>
2019-06-24 23:29:15 +0800

    Create Statistics_from_a_Large_Sample.cpp

---
+++ Statistics_from_a_Large_Sample.cpp
@@ -0,0 +1,45 @@
+//Leetcode 1093
+
+class Solution {
+public:
+    vector<double> sampleStats(vector<int>& count) {
+        long long sum=0,mi=INT_MAX,ma=INT_MIN;
+        int mo=-1,mo_count=0;
+        vector<int> vv(count.size(),0);
+        vv[0] = count[0];
+        for(int i=0;i<count.size();++i) {
+            sum+=i*count[i];
+            if(count[i] > 0) {
+                if(mi == INT_MAX) mi = i;
+                ma = i;
+            }
+            if(count[i]>mo_count) {
+                mo_count = count[i];
+                mo = i;
+            }
+            if(i>0) vv[i] = vv[i-1] + count[i];
+        }
+        double mean = sum * 1.0 / vv[count.size()-1];
+        double median = -1;
+        if(count.size() % 2 == 1) {
+            int mind = (vv.back()+1)/2;
+            int ind = lower_bound(vv.begin(), vv.end(), mind)-vv.begin();
+            median = ind;
+        }
+        else {
+            int mind1 = vv.back()/2;
+            int mind2 = mind1+1;
+            int ind1 = lower_bound(vv.begin(), vv.end(), mind1)-vv.begin();
+            int ind2 = lower_bound(vv.begin(), vv.end(), mind2)-vv.begin();
+            //cout<<ind1<<"\t"<<ind2<<endl;
+            median = (ind1+ ind2) * 1.0 / 2;
+        }
+        vector<double> res(5);
+        res[0] = mi;
+        res[1] = ma;
+        res[2] = mean;
+        res[3] = median;
+        res[4] = mo;
+        return res;
+    }
+};
