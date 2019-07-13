3b3d431905ede8e86f28c772825429876a4123c4
HaoYang0123 <yhloverain@gmail.com>
2019-07-13 12:14:40 +0800

    Create Merge_Intervals.cpp

---
+++ Merge_Intervals.cpp
@@ -0,0 +1,35 @@
+//Leetcode 56
+
+bool cmp(const vector<int> & a, const vector<int> & b) {
+    if(a[0]==b[0]) return a[1]<b[1];
+    return a[0]<b[0];
+}
+
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        int n=intervals.size();
+        if(n==0) return intervals;
+        sort(intervals.begin(), intervals.end(), cmp);
+        //for(int i=0;i<n;++i) cout<<intervals[i][0]<<"\t"<<intervals[i][1]<<endl;
+        vector<vector<int>> res;
+        int a=intervals[0][0],b=intervals[0][1];
+        for(int i=1;i<n;++i) {
+            //cout<<intervals[i][0]<<"\t"<<intervals[i][1]<<"\t"<<a<<"\t"<<b<<endl;
+            if(intervals[i][0]<=b) b=max(b,intervals[i][1]);
+            else {
+                vector<int> tmp(2,0);
+                tmp[0]=a;
+                tmp[1]=b;
+                res.push_back(tmp);
+                a=intervals[i][0];
+                b=intervals[i][1];
+            }
+        }
+        vector<int> tmp(2,0);
+        tmp[0]=a;
+        tmp[1]=b;
+        res.push_back(tmp);
+        return res;
+    }
+};
