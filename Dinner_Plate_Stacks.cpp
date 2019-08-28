bb99181ff509f682c1b23a932305f5c5af19f618
HaoYang0123 <yhloverain@gmail.com>
2019-08-28 22:31:57 +0800

    Update Dinner_Plate_Stacks.cpp

--- Dinner_Plate_Stacks.cpp
+++ Dinner_Plate_Stacks.cpp
@@ -1,6 +1,7 @@
 //Leetcode 1172
 //使用两个set居然超时了，还不如网上用的vector？没明白为何这个速度不行
 
+//方法1：超时
 class DinnerPlates {
 public:
     vector<stack<int>> data;
@@ -58,10 +59,57 @@ public:
     }
 };
 
-/**
- * Your DinnerPlates object will be instantiated and called as such:
- * DinnerPlates* obj = new DinnerPlates(capacity);
- * obj->push(val);
- * int param_2 = obj->pop();
- * int param_3 = obj->popAtStack(index);
- */
+//方法2：网上代码，不超时。感觉是由于案例的stack的capacity是2，很少所以经常会调用set的更新，而这个代码不会
+class DinnerPlates {
+public:
+    vector<vector<int>> v;
+    int c, max, nextr;
+    
+    DinnerPlates(int capacity) {
+        c = capacity;
+        v = vector<vector<int>>(100002, vector<int>());
+        nextr = 0;
+        max = 0;
+    }
+    
+    void push(int val) {
+        v[nextr].push_back(val);
+
+        while(nextr < v.size() && v[nextr].size() == c){
+            nextr++;
+        }
+        if(nextr > max){
+            //correctly set value for max;
+            max = (v[nextr].size() > 0) ? nextr : nextr-1;
+        }
+    }
+    
+    int pop() {
+        int s = v[max].size();
+        if(max <= 0 && s == 0){
+            return -1;
+        }
+        int t = v[max][s-1];
+        v[max].pop_back();
+        while(v[max].size() == 0 && max > 0){
+            max--;
+        }
+        if(nextr > max) nextr = max;
+        return t;
+    }
+    
+    int popAtStack(int index) {
+        int s = v[index].size();
+        if(s == 0){
+            return -1;
+        }
+        int t = v[index][s-1]; 
+        v[index].pop_back();
+        if(index < nextr) nextr = index;
+        if(v[index].size() == 0 && index == max && max > 0){
+            max--;
+        }
+        return t;
+    }
+};
+
