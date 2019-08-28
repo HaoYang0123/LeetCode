7685c19d05aa938eba1a0db7865d911e85345744
HaoYang0123 <yhloverain@gmail.com>
2019-08-28 21:01:54 +0800

    Update Max_Points_On_A_Line.cpp

--- Max_Points_On_A_Line.cpp
+++ Max_Points_On_A_Line.cpp
@@ -14,7 +14,8 @@ public:
     int maxPoints(vector<vector<int>>& points) {
         if(points.size()==0) return 0;
         int res=1;
-        sort(points.begin(), points.end());
+        sort(points.begin(), points.end()); //一定的排序，比如(1,1),(0,0),(2,2)本来是共线的
+        //如果不排序(1,1)为基点，那么会导致(0,0)与(2,2)算出来的斜率不一样(分别是(1,1)和(-1,-1))
         for(int i=0;i<points.size()-1;++i) {
             int cur=1; //points[i]的数量
             map<pair<int,int>, int> mm;
