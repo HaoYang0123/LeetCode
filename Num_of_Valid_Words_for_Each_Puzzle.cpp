e181571ea670bde6f63634b0897b9d20ef00fe76
HaoYang0123 <yhloverain@gmail.com>
2019-09-05 19:59:14 +0800

    Update Num_of_Valid_Words_for_Each_Puzzle.cpp

--- Num_of_Valid_Words_for_Each_Puzzle.cpp
+++ Num_of_Valid_Words_for_Each_Puzzle.cpp
@@ -34,3 +34,64 @@ public:
 		return res;
 	}
 };
+
+//方法2：网上的代码，位运算
+class Solution {
+public:
+    vector<int> findNumOfValidWords(vector<string>& words, vector<string>& puzzles) {
+        // Lengths
+        int P = puzzles.size();
+        int W = words.size();
+        
+        // Letter masks
+        vector<int> letterMask(26, 0); //相当于'a'表示为1,'b'表示为10,'c'表示为100
+        
+        for (int i = 0, mask = 1; i < 26; ++i, mask <<= 1) {
+            letterMask[i] = mask;
+        }
+        
+        // Word masks with count info
+        unordered_map<int, int> wordsMask;
+        
+        // For each word
+        for (int i = 0; i < W; ++i) {
+            // Compute word mask
+            int mask = 0; //mask表示words[i]对应的hash值，只有“相同”字符串的才有相同的hash值
+			//这里的“相同”表示为"abc"和"cba"也认为是“相同”
+            
+            for (char c: words[i]) {
+                mask = mask | letterMask[c - 'a'];
+            }
+            
+            ++wordsMask[mask];
+        }
+       
+         // Result : result[i] => number of words covered by puzzles[i]
+        vector<int> result(P, 0); 
+        
+        // For each puzzle
+        for (int i = 0; i < P; ++i) {
+            // Compute puzzle mask
+            int mask = 0; //mask表示为puzzles[i]对应的hash值
+            
+            for (char c: puzzles[i]) {
+                mask = mask | letterMask[c - 'a'];
+            }
+            
+            // Iterate through all valid subset of mask i.e. subset of puzzle chars = 2^7 = 64 in count
+            // This makes the complexity O(P*64)
+            int subMask = mask;
+            
+            while (subMask) {
+                if ((subMask & letterMask[puzzles[i][0] - 'a']) && wordsMask.count(subMask)) {
+                    result[i] += wordsMask[subMask];
+                }
+                
+                // Only select valid bits i.e. bits corresponding to chars in puzzle
+                subMask = (subMask - 1) & mask;
+            }
+        }
+        
+        return result;
+    }
+};
