4c389bfc4da334f32bd2d86b6927a7f28677c1e1
HaoYang0123 <yhloverain@gmail.com>
2019-11-14 20:21:22 +0800

    Update Sliding_Window_Maximum.cpp

--- Sliding_Window_Maximum.cpp
+++ Sliding_Window_Maximum.cpp
@@ -1,27 +1,15 @@
-//Leetcode 239
-
 class Solution {
 public:
-    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        map<int,int> win;
-        int n=nums.size();
-        if(n==0) return vector<int>();
-        int cur_max = INT_MIN;
-        for(int i=0;i<k;++i) {
-            win[nums[i]]++;
-            cur_max = max(cur_max, nums[i]);
-        }
-        vector<int> res(n-k+1, 0);
-        res[0] = cur_max;
-        for(int i=k;i<n;++i) {
-            win[nums[i-k]]--;
-            win[nums[i]]++;
-            auto ind = win.rbegin();
-            while(ind != win.rend() && ind->second == 0) {
-                ++ind;
-            }
-            res[i+1-k] = ind->first;
-        }
-        return res;
-    }
+	vector<int> maxSlidingWindow(vector<int> & nums, int k) {
+		map<int,int> val2num;
+		for(int i=0;i<k-1;++i) val2num[nums[i]]++;
+		vector<int> res;
+		for(int i=k-1;i<nums.size();++i) {
+			val2num[nums[i]]++;
+			res.push_back(val2num.rbegin()->first);
+			val2num[nums[i-k+1]]--;
+			if(val2num[nums[i-k+1]] == 0) val2num.erase(nums[i-k+1]);
+		}
+		return res;
+	}
 };
