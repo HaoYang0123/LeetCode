50c32c4d0c63d77b8737688dc3e428395cb7c392
HaoYang0123 <yhloverain@gmail.com>
2019-11-15 20:05:04 +0800

    Update Find_Median_from_Data_Stream.cpp

--- Find_Median_from_Data_Stream.cpp
+++ Find_Median_from_Data_Stream.cpp
@@ -1,34 +1,66 @@
 //Leetcode 295
-//使用map来记录数据
-//几乎超时
+//方法1：使用multiset来记录数据，几乎超时
 
 class MedianFinder {
 public:
-	map<int,int> val2num;
-	int count;
+	multiset<int> ss;
 	MedianFinder() {
-		count = 0;
 	}
 
 	void addNum(int num) {
-		val2num[num]++;
-		count++;
+		ss.insert(num);
 	}
 
 	double findMedian() {
-		bool is_odd = count % 2;
-		int median_k = count / 2; //median position
-		auto start_ind = val2num.begin();
-		int low = 0, high = 0;
-		while(true) {
-			high = low + start_ind->second;
-			if(low <= median_k && high >= median_k + is_odd) break;
-			start_ind++;
-			low = high;
+		if(ss.size()==0) return 0.0;
+		int median = ss.size() / 2;
+		auto ind = ss.begin();
+		for(int j=0;j<median;++j) ind++;
+		if(ss.size() % 2) return *ind;
+		return ((long long)(*ind) + (long long)(*(--ind))) / 2.0;
+	}
+};
+
+//方法2：同样使用multiset来记录，但是增加一个变量median_index记录当前的中间位置（如果数目是偶数，记录median中的较大值）
+class MedianFinder {
+public:
+	multiset<int> ss;
+	multiset<int>::iterator median_index;
+	MedianFinder() {
+	}
+
+	void addNum(int num) {
+		if(ss.size() == 0) { //如果当前没有元素，直接加进去，median_index就是当前加进去的位置
+			ss.insert(num);
+			median_index = ss.begin();
+			return;
 		}
-		if(is_odd) return start_ind->first;
-		if(high == median_k)
-			return ((long long)(start_ind->first)+(long long)((++start_ind)->first)) / 2.0;
-		return start_ind->first;
+		if(ss.size() % 2) { // has odd size
+			int median_value = *median_index;
+			ss.insert(num);
+			if(num >= median_value) ++median_index; //如果当前增加的元素大于等于median_value，则更新median_index【注：等于也需要更新】
+		}
+		else { // has even size
+			int median_value_big = *median_index;
+			auto index2 = median_index;
+			--index2;
+			int median_value_small = *index2;
+			if(num < median_value_big && num >= median_value_small) {
+				ss.insert(num);
+				--median_index;
+			}
+			else if(num < median_value_small) {
+				--median_index;
+				ss.insert(num);
+			}
+			else ss.insert(num);
+		}
+	}
+
+	double findMedian() {
+		if(ss.size() % 2) return *median_index;
+		auto index2 = median_index;
+		--index2;
+		return ((long long)(*median_index) + (long long)(*index2)) / 2.0;
 	}
 };
