2d26f74b2c1ac677ced69e7c7d57490eab8b8a02
HaoYang0123 <yhloverain@gmail.com>
2019-12-02 19:48:05 +0800

    Update Count_Square_Submatrices_with_All_Ones.cpp

--- Count_Square_Submatrices_with_All_Ones.cpp
+++ Count_Square_Submatrices_with_All_Ones.cpp
@@ -1,6 +1,6 @@
 //Leetcode 1277
-//O(N^3)时间复杂度
 
+//方法1：O(N^3)时间复杂度
 class Solution {
 public:
 	int countSquares(vector<vector<int>> & matrix) {
@@ -26,6 +26,25 @@ public:
 				}
 				int max_side = (k-j);
 				res += max_side;
+			}
+		}
+		return res;
+	}
+};
+
+//方法2，时间复杂度为O(N^2)
+class Solution {
+public:
+	int countSquares(vector<vector<int>> & matrix) {
+		int n=matrix.size(),m=matrix[0].size();
+		vector<vector<int>> dp(n+1,vector<int>(m+1,0)); //dp[i][j]表示以(i,j)作为正方形的右下角，正方形的数量
+		int res = 0;
+		for(int i=0;i<n;++i) {
+			for(int j=0;j<m;++j) {
+				if(matrix[i][j] == 1) {
+					dp[i+1][j+1] = 1+min(dp[i][j+1],dp[i+1][j],dp[i][j]);
+					res += dp[i+1][j+1];
+				}
 			}
 		}
 		return res;
