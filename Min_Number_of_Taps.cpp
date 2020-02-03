9ee6db828fce2211483be5c1620789a9bdeb4d90
HaoYang0123 <yhloverain@gmail.com>
2020-02-03 12:51:20 +0800

    Update Min_Number_of_Taps.cpp

--- Min_Number_of_Taps.cpp
+++ Min_Number_of_Taps.cpp
@@ -1,5 +1,5 @@
 //Leetcode 1326
-
+//方法1：使用map从小到大记录所有的interval
 class Solution {
 public:
     int minTaps(int n, vector<int>& ranges) {
@@ -22,6 +22,40 @@ public:
             if(cur_end == max_end) break;
             ++res;
             max_end = cur_end;
+        }
+        if(max_end < n) return -1;
+        return res;
+    }
+};
+
+//方法2：使用vector来存储，效率更高，时间复杂度为O(N)
+class Solution {
+public:
+    int minTaps(int n, vector<int>& ranges) {
+        vector<pair<int,int>> intervals;
+        for(int i=0;i<ranges.size();++i) {
+            if(ranges[i] == 0) continue;
+            int start = max(0, i - ranges[i]);
+            int end = min(n, i + ranges[i]);
+            intervals.push_back(pair<int,int>(start,end));
+        }
+        sort(intervals.begin(), intervals.end(), [](auto & a, auto & b){
+            if(a.first == b.first) return a.second > b.second;
+            return a.first < b.first;
+        });
+        if(intervals.size() == 0) return -1;
+        if(intervals[0].first != 0) return -1;
+        int max_end = intervals[0].second;
+        int res = 1;
+        for(int i=1;i<intervals.size();) {
+            int cur_end = max_end;
+            while(i<intervals.size() && intervals[i].first <= max_end) {
+                cur_end = max(cur_end, intervals[i].second);
+                ++i;
+            }
+            if(cur_end == max_end) break;
+            max_end = cur_end;
+            ++res;
         }
         if(max_end < n) return -1;
         return res;
