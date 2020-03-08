d59de3703a94786543e25c26563ae0ac34f1e2d9
HaoYang0123 <yhloverain@gmail.com>
2020-03-08 13:52:42 +0800

    Update Rank_Teams_by_Votes.cpp

--- Rank_Teams_by_Votes.cpp
+++ Rank_Teams_by_Votes.cpp
@@ -39,3 +39,35 @@ public:
         return string(tmp);
     }
 };
+
+//stable_sort，进行稳定排序
+
+bool cmp(const pair<int,vector<int>> & a, const pair<int,vector<int>> & b) {
+    for(int i=0;i<26;++i) {
+        if(a.second[i]<b.second[i]) return false;
+        else if(a.second[i]>b.second[i]) return true;
+    }
+    return false;
+}
+
+class Solution {
+public:
+    string rankTeams(vector<string>& votes) {
+        vector<pair<int, vector<int>>> data;
+        for(int i=0;i<26;++i) {
+            data.push_back(pair<int, vector<int>>(i, vector<int>(26,0)));
+        }
+        for(int i=0;i<votes.size();++i) {
+            string & v = votes[i];
+            for(int j=0;j<v.length();++j) {
+                data[int(v[j]-'A')].second[j]++;
+            }
+        }
+        stable_sort(data.begin(),data.end(),cmp);
+        char tmp[27];
+        int i=0;
+        for(;i<votes[0].length();++i) tmp[i] = char(data[i].first+'A');
+        tmp[i] = '\0';
+        return string(tmp);
+    }
+};
