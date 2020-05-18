4950889249c0e566ec2b1508f17a33fef074c397
HaoYang0123 <yhloverain@gmail.com>
2020-05-18 22:02:29 +0800

    Update People_Whose_List_of_Fav.cpp

--- People_Whose_List_of_Fav.cpp
+++ People_Whose_List_of_Fav.cpp
@@ -12,7 +12,8 @@ public:
                     company2index[fav[j]] = company2index.size();
             }
         }
-        vector<set<int>> fav_com(favoriteCompanies.size(), set<int>());
+        //使用unodered_set比使用set快许多
+        vector<unordered_set<int>> fav_com(favoriteCompanies.size(), unordered_set<int>());
         for(int i=0;i<favoriteCompanies.size();++i) {
             vector<string> & fav = favoriteCompanies[i];
             for(int j=0;j<fav.size();++j) {
@@ -20,12 +21,6 @@ public:
             }
         }
         
-        /*
-        for(int i=0;i<fav_com.size();++i) {
-            for(auto ind = fav_com[i].begin(); ind != fav_com[i].end(); ++ind) cout<<*ind<<"\t";
-            cout<<endl;
-        }
-        */
         vector<int> res;
         for(int i=0;i<fav_com.size();++i) {
             if(!find_is_subset(fav_com, i)) res.push_back(i);
@@ -33,7 +28,7 @@ public:
         return res;
     }
     
-    inline bool find_is_subset(vector<set<int>> & fav, int com) {
+    inline bool find_is_subset(vector<unordered_set<int>> & fav, int com) {
         for(int i=0;i<fav.size(); ++i) {
             if(i == com) continue;
             if(b_is_set_of_a(fav[i], fav[com])) return true;
@@ -41,7 +36,7 @@ public:
         return false;
     }
     
-    inline bool b_is_set_of_a(set<int> & a, set<int> & b) {
+    inline bool b_is_set_of_a(unordered_set<int> & a, unordered_set<int> & b) {
         for(auto ind=b.begin(); ind != b.end(); ++ind) {
             if(a.find(*ind) == a.end()) return false;
         }
