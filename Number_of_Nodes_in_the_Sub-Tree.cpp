8555b2cff863f38199708b492be68c8204d3d7fd
HaoYang0123 <yhloverain@gmail.com>
2020-07-19 23:35:33 +0800

    Update Number_of_Nodes_in_the_Sub-Tree.cpp

--- Number_of_Nodes_in_the_Sub-Tree.cpp
+++ Number_of_Nodes_in_the_Sub-Tree.cpp
@@ -1,35 +1,33 @@
 //Leetcode 1519
-//自己写的WA，因为我没有对edges进行处理，有些例子，需要根据edges转化成树（知道哪个结点才是根结点）
 
 class Solution {
 public:
+    vector<int> final_res;
     vector<int> countSubTrees(int n, vector<vector<int>>& edges, string labels) {
         vector<vector<int>> dp(n, vector<int>(26,0)); //dp[i]记录第i个结点包含各字符的数量
         vector<bool> is_vis(n, false);
-        vector<int> res(n, 0);
+        final_res.resize(n);
         vector<vector<int>> graph(n, vector<int>());
         for(int i=0;i<edges.size();++i) {
-            graph[edges[i][0]].push_back(edges[i][1]); //【注：这里有些问题】
+            graph[edges[i][0]].push_back(edges[i][1]); //注：这里是无向图
+            graph[edges[i][1]].push_back(edges[i][0]);
         }
-        for(int i=0;i<n;++i) {
-            res[i] = dfs(i, graph, labels, dp, is_vis);
-        }
-        return res;
+        dfs(0, graph, labels, dp, is_vis); //根据题意，结点0为根结点，从根结点使用深度搜索，计算dp
+        return final_res;
     }
-    inline int dfs(int root, vector<vector<int>> & graph, string & labels, vector<vector<int>> & dp, vector<bool> & is_vis) {
+    inline void dfs(int root, vector<vector<int>> & graph, string & labels, vector<vector<int>> & dp, vector<bool> & is_vis) {
         int cur_c = int(labels[root]-'a');
-        if(is_vis[root]) return dp[root][cur_c];
-        vector<int> edges = graph[root];
-        for(int c = 0; c < 26; ++c) {
-            int res = 0;
-            if(c == cur_c) res = 1;
-            for(int i=0; i<edges.size(); ++i) {
-                dfs(edges[i], graph, labels, dp, is_vis);
-                res += dp[edges[i]][c];
-            }
-            dp[root][c] = res;
-        }
+        if(is_vis[root]) return ;
         is_vis[root] = true;
-        return dp[root][cur_c];
+        vector<int> & edges = graph[root];
+        dp[root][cur_c] = 1;
+        for(int i=0; i<edges.size(); ++i) { //枚举所有的子树
+            if(is_vis[edges[i]]) continue; //如果计算过，则不考虑
+            dfs(edges[i], graph, labels, dp, is_vis);
+            for(int c = 0; c < 26; ++c) { //更新所有的字符情况
+                dp[root][c] += dp[edges[i]][c];
+            }
+        }
+        final_res[root] = dp[root][cur_c];
     }
 };
