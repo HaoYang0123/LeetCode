1c765bbbe8ee1773a643270b57316fa123e25b01
HaoYang0123 <yhloverain@gmail.com>
2020-12-12 11:04:33 +0800

    Create Richest_Customer_Wealth.cpp

---
+++ Richest_Customer_Wealth.cpp
@@ -0,0 +1,15 @@
+//Leetcode 1672
+
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+        int res = 0;
+        for(int i=0; i<accounts.size(); ++i) {
+            int c = 0;
+            for(int j=0; j<accounts[i].size(); ++j)
+                c += accounts[i][j];
+            res = max(res, c);
+        }
+        return res;
+    }
+};
