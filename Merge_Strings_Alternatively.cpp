bd04b84419790b2c2d01115cacd1b30b899eff47
HaoYang0123 <yhloverain@gmail.com>
2021-02-27 20:56:36 +0800

    Create Merge_Strings_Alternatively.cpp

---
+++ Merge_Strings_Alternatively.cpp
@@ -0,0 +1,16 @@
+// Leetcode 1768
+
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        string res = "";
+        int i = 0, j = 0;
+        while(i < word1.length() && j < word2.length()) {
+            res += word1[i++];
+            res += word2[j++];
+        }
+        while(i < word1.length()) res += word1[i++];
+        while(j < word2.length()) res += word2[j++];
+        return res;
+    }
+};
