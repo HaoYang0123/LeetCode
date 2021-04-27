5387aace92143899fa9ad64a9317eeaf6da02853
HaoYang0123 <yhloverain@gmail.com>
2021-04-27 20:59:01 +0800

    Update Frequency_of_Most_Frequent_Element.cpp

--- Frequency_of_Most_Frequent_Element.cpp
+++ Frequency_of_Most_Frequent_Element.cpp
@@ -7,19 +7,16 @@ public:
 		int res = 1;
 		sort(nums.begin(), nums.end());
 		int i = 0; //从i至j
-		long long cur_sum = nums[0];
-		for (int j = 0; j < nums.size(); ) {
+		long long cur_sum = 0;
+		for (int j = 0; j < nums.size(); ++j) {
+			cur_sum += nums[j];
 			long long new_sum = (j - i + 1) * (long long)nums[j];
-			if (new_sum - cur_sum > k) {  //如果(i,j)的数组转成等于元素都为nums[j]的子数组，需要的步数超过了k，则++i并更新cur_sum
+			while (new_sum - cur_sum > k) {  //如果(i,j)的数组转成等于元素都为nums[j]的子数组，需要的步数超过了k，则++i并更新cur_sum
 				cur_sum -= nums[i];
 				++i;
+				new_sum = (j - i + 1) * (long long)nums[j];
 			}
-			else {  //否则，可以继续扩大j
-				res = max(res, j - i + 1);
-				++j;
-				if (j >= nums.size()) break;
-				cur_sum += nums[j];
-			}
+			res = max(res, j - i + 1);
 		}
 		return res;
 	}
