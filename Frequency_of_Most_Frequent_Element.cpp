d282a9ccfd4da9f19316c555ffc9ccbd18e552d7
HaoYang0123 <yhloverain@gmail.com>
2021-04-27 20:55:22 +0800

    Create Frequency_of_Most_Frequent_Element.cpp

---
+++ Frequency_of_Most_Frequent_Element.cpp
@@ -0,0 +1,26 @@
+// Leetcode 1838
+//需要进行排序，所以时间复杂度为O(NlgN)
+
+class Solution {
+public:
+	int maxFrequency(vector<int>& nums, int k) {
+		int res = 1;
+		sort(nums.begin(), nums.end());
+		int i = 0; //从i至j
+		long long cur_sum = nums[0];
+		for (int j = 0; j < nums.size(); ) {
+			long long new_sum = (j - i + 1) * (long long)nums[j];
+			if (new_sum - cur_sum > k) {  //如果(i,j)的数组转成等于元素都为nums[j]的子数组，需要的步数超过了k，则++i并更新cur_sum
+				cur_sum -= nums[i];
+				++i;
+			}
+			else {  //否则，可以继续扩大j
+				res = max(res, j - i + 1);
+				++j;
+				if (j >= nums.size()) break;
+				cur_sum += nums[j];
+			}
+		}
+		return res;
+	}
+};
