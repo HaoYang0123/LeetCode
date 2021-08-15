b59631f481f89760040a39a4537c81e54380bb3f
HaoYang0123 <yhloverain@gmail.com>
2021-08-15 21:13:04 +0800

    Create Delete_Characters_to_Make_Fancy_String.cpp

---
+++ Delete_Characters_to_Make_Fancy_String.cpp
@@ -0,0 +1,15 @@
+// Leetcode 1957
+
+class Solution {
+public:
+    bool isThree(int n) {
+        set<int> div;
+        for(int i=1; i<=n/2;++i) {
+            if(n % i == 0) {
+                div.insert(i);
+                div.insert(n/i);
+            }
+        }
+        return div.size() == 3;
+    }
+};
