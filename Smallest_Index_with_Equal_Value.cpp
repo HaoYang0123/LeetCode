0a8fb49acfa5432754743678bc4c642ce583443a
HaoYang0123 <yhloverain@gmail.com>
2021-12-12 14:09:20 +0800

    Create Smallest_Index_with_Equal_Value.cpp

---
+++ Smallest_Index_with_Equal_Value.cpp
@@ -0,0 +1,11 @@
+// Leetcode 2057
+
+class Solution {
+public:
+    int smallestEqual(vector<int>& nums) {
+        for(int i=0; i<nums.size(); ++i) {
+            if(i%10 == nums[i]) return i;
+        }
+        return -1;
+    }
+};
